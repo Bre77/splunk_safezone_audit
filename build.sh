2b6036afecb836c8bc62824c5d7198784a8356db
Brett Adams <brett.whynot@gmail.com>
2025-11-27 11:32:51 +1000

    v1.0.0

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
 ucc-gen build --ta-version $1
 find output/safezone_audit -name ".*" -type f -delete
+find output/safezone_audit -name "__pycache__" -type d -exec rm -rf {} +
 ucc-gen package --path output/safezone_audit
